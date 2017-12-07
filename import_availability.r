91d9dcfd3481a57605485770b45fd071b1d47277
Imtiaz Ahmed Nizami <imtiaz.nizami@telenor.com.pk>
2017-12-07 11:12:43 +0500

    1. Included code to delete data for locked sites. 2. Included code to count rows. 3. Added comments.

--- import_availability.r
+++ import_availability.r
@@ -1,10 +1,19 @@
 library(sqldf)
 
 setwd("d:/optimization/stats")
-t_availability <- read.csv("tbl_2G_availability.csv")
 
+#--- import data from file
+t_availability <- read.csv("tbl_2G_availability.csv")
 db <- dbConnect(SQLite(), dbname="kpi_south.sqlite")
 dbWriteTable(conn = db, name = "t_availability", value = t_availability, row.names = FALSE, header = TRUE, append = TRUE)
+
+#--- delete rows where site is locked
+s_qry = paste("DELETE FROM t_availability WHERE \"SITE.Name\" LIKE \"%LCK%\"")
+dbGetQuery(db, s_qry)
+
+#--- count rows in table
+s_qry = paste("select count(*) from t_availability")
+delme <- dbGetQuery(db, s_qry)
 
 dbDisconnect(db)            # Close connection
 #rm(list = c("t_availability"))   # Remove data frames
