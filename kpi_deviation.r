5e07aa4180a1acaa6f80cf8750903b6da4c42fa0
Imtiaz Ahmed Nizami <imtiaz.nizami@telenor.com.pk>
2018-02-28 16:47:25 +0500

    Changed query to include about 40 days of data. Included and then commented code if traffic deviation is required.

--- kpi_deviation.r
+++ kpi_deviation.r
@@ -13,9 +13,14 @@ db <- dbConnect(SQLite(), dbname="kpi_south.sqlite")
 
 # constants
 OBS_DAYS = 10
-OBS_SECS = 3600 * 24 * 8
+OBS_SECS = 3600 * 24 * OBS_DAYS
 
-s_qry = paste("SELECT * FROM t_kpi_deviation WHERE \"BTS.NAME\" IS NOT NULL")
+max_date = dbGetQuery(db, "SELECT max(\"Start.Time\") from t_kpi_deviation")
+max_date = as.POSIXct(max_date[,c(1)], tz="GMT", format="%Y-%m-%d %H:%M:%S")
+max_date = max_date - OBS_SECS * 4
+#max_date = as.POSIXlt(max_date)
+s_qry = paste("SELECT * FROM t_kpi_deviation WHERE \"BTS.NAME\" IS NOT NULL AND \"Start.Time\" > '", as.character(max_date), "'", sep="")
+#s_qry = paste("SELECT * FROM t_kpi_deviation WHERE \"BTS.NAME\" IS NOT NULL AND Start.Time > %2018-01-17%")
 df_kpi_all = dbGetQuery(db, s_qry)
 
 # delete sectors where observation counts are less than changepoint analysis segment length
@@ -26,11 +31,14 @@ df_sectors = df_kpi_all %>% select(BTS.NAME) %>% distinct() %>% filter(!grepl(st
 
 # initializations
 v_sectors = df_sectors$BTS.NAME
-v_penatly = c(30, 40, 50, 100)
-df = data.frame(sector=character(),priority=character())
+#v_penatly = c(50, 100, 150, 200) # traffic
+v_penatly = c(30, 40, 50, 80) # mpd
+df = data.frame(sector=character(),priority=character(),changedate=character())
 approx_iterations = as.integer(nrow(df_sectors) * (.001 + 0.01 + 0.06 + 0.03 + 1 + .3))
 pb = txtProgressBar(min = 0, max = approx_iterations, style = 3)
 pb_count = 0
+
+#df_reduced <- df_kpi_all %>% select("Start.Time", "BTS.NAME", SHH_TCH_Traffic)
 
 for ( k in 1:length(v_penatly) ) {
   vec <- vector()
@@ -40,7 +48,8 @@ for ( k in 1:length(v_penatly) ) {
     pb_count = pb_count + 1
     setTxtProgressBar(pb, pb_count)
 
-    df_reduced <- df_kpi_all %>% filter(df_kpi_all$BTS.NAME == v_sectors[i]) %>% select("Start.Time", SHH_MPD)
+    #df_reduced <- df_kpi_all %>% filter(df_kpi_all$BTS.NAME == v_sectors[i]) %>% select("Start.Time", SHH_TCH_Traffic) # traffic
+    df_reduced <- df_kpi_all %>% filter(df_kpi_all$BTS.NAME == v_sectors[i]) %>% select("Start.Time", SHH_MPD) # mpd
     colnames(df_reduced) <- c("timestamp", "count")
     df_reduced[[1]] <- as.POSIXct(df_reduced[,c(1)], tz="GMT", format="%Y-%m-%d %H:%M:%S")
 
@@ -59,8 +68,8 @@ for ( k in 1:length(v_penatly) ) {
           index <- df$sector == v_sectors[i]
           df$priority[index] = v_penatly[k]
         } else {
-          df_temp = data.frame(v_sectors[i], v_penatly[k])
-          names(df_temp) = c("sector", "priority")
+          df_temp = data.frame(v_sectors[i], v_penatly[k], df_reduced[cpts(res.cptmean),1])
+          names(df_temp) = c("sector", "priority", "changedate")
           df = rbind(df, df_temp)
         }
 
@@ -72,3 +81,8 @@ for ( k in 1:length(v_penatly) ) {
   v_sectors = vec
 
 } # end penalty for loop
+
+setTxtProgressBar(pb, approx_iterations)
+
+fn = paste("d:/optimization/stats/kpi_dev_output_", format(Sys.time(), "%Y%m%d%H%M%S"), ".csv", sep="")
+write.csv(df, file = fn)
